67970e7d128302869b961b5db9ccddf6a5c10e7d
Anastasia <zumwolfzum@gmail.com>
2021-01-08 21:47:50 +0300

    ашкые firs commit

---
+++ src/lesson2/Currency.java
@@ -0,0 +1,24 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Currency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите cумму");
+        int rubles = scanner.nextInt();
+
+        if (rubles % 10 == 1 && rubles % 100 != 11) {
+
+        } else if ((rubles % 10 >= 2 && rubles % 10 <= 4)
+                && !(rubles % 100 >= 12 && rubles % 100 <= 14)) {
+            System.out.println(rubles + " рубля");
+        } else {
+            System.out.println(rubles + " рублей");
+        }
+    }
+}
+
+
+
---
+++ src/lesson2/Season.java
@@ -0,0 +1,25 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Season {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите порядковый номер месяца");
+        int inputValue = scanner.nextInt();
+
+        if (inputValue == 1 || inputValue == 2 || inputValue == 12) {
+
+            System.out.println("Зима");
+
+        } else if (3 <= inputValue && inputValue <= 5) {
+            System.out.println("Весна");
+        } else if (6<= inputValue && inputValue <= 8) {
+            System.out.println("Лето");
+        } else if (9 <= inputValue && inputValue <= 11){
+            System.out.println("Осень");
+        } else
+        System.out.println("Нет такого месяца");
+    }
+}
